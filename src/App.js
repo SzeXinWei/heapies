51a720d9aff3e85a093f1f8fec73bfe1b1348ab2
SzeXinWei <xinweisze@gmail.com>
2022-07-07 16:40:54 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Login from './Login'
+import React from "react";
+
+function App() {
+  return <Login />
+    
+}
+
+export default App;
---
+++ src/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './login.css'
+
+function Login() {
+  return (
+    <div className = "login">
+      <h1>LET US IN, PLEASE</h1>
+      <a>LOGIN</a>
+    </div>
+  )
+}
+
+export default Login
+
+
+

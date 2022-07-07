ad3da83a737410a235f5ffab107b69b6a0b1491a
laexxtia <laetitiahongje@gmail.com>
2022-07-07 17:55:04 +0800

    second draft - with image path changed to public

--- src/Helloconnected.js
+++ src/Helloconnected.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Helloconnected.css'
-import wink from './images/wink.png'
-import logo from './images/spotifylogo.png'
+import wink from '../public/images/wink.png'
+import logo from '../public/images/spotifylogo.png'
 
 function HelloName(){
     return (

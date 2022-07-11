9097c40eb0fcfa6c1a13ac18c817f3c7f8e7e542
laexxtia <laetitiahongje@gmail.com>
2022-07-11 17:18:28 +0800

    need help with css for play button

---
+++ src/Playlistplay.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import './Playlistplay.css'
+import circle from './images/circle.png'
+
+export function NamesPlaylist(){
+    return(
+        <h1 className='namesplaylist'>Name's playlist &lt;3 </h1>
+    )
+}
+
+export function Song(){
+    return(
+        <div className='song'>
+            <img className='c' src={circle} />
+            <p className='s'>SONG</p>
+            <p className='a'>ARTIST</p>
+            <p className='num'>1</p>
+        </div>
+    )
+}
+
+// based on how many songs are generated
+
+function Play(){
+    return(
+        <p className='play'>PLAY</p>
+    )
+}
+
+function Save(){
+    return(
+        <p className='save'>SAVE IN SPOTIFY</p>
+    )
+}
+
+function Playlistplay(){
+    return(
+        <div>
+            <NamesPlaylist />
+            <Song />
+            <Play />
+            <Save />
+        </div>
+    )
+}
+
+export default Playlistplay
